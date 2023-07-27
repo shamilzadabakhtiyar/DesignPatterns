4276fed21e64f1e4b53621fdc6925b43ee3fdf52
Admin <Admin@DESKTOP-34CAHMM>
2023-07-27 12:33:17 +0400

    Add project files.

---
+++ DesignPatterns.SingletonPattern/CountryProvider.cs
@@ -0,0 +1,30 @@
+﻿using DesignPatterns.SingletonPattern.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.SingletonPattern
+{
+    public class CountryProvider
+    {
+        private static CountryProvider instance;
+        public static CountryProvider Instance => instance ?? (instance = new CountryProvider());
+        private new List<Country> Countries { get; set; }
+
+        private CountryProvider()
+        {
+            Task.Delay(2000).GetAwaiter().GetResult();
+            Countries = new List<Country>()
+                {
+                    new Country(){Name = "Az"},
+                    new Country(){Name = "Tr"}
+                };
+        }
+
+        public int CountryCount => Countries.Count;
+        public async Task<List<Country>> GetCountries() => Countries;
+    }
+}
---
+++ DesignPatterns.SingletonPattern/Program.cs
@@ -0,0 +1,11 @@
+﻿using DesignPatterns.SingletonPattern;
+
+Console.WriteLine(DateTime.Now.ToLongTimeString());
+var countries = await CountryProvider.Instance.GetCountries();
+//var countries = countryProvider.GetCountries().GetAwaiter().GetResult();
+
+foreach (var country in countries)
+    Console.WriteLine(country.Name);
+
+var countries2 = await CountryProvider.Instance.GetCountries();
+Console.WriteLine(DateTime.Now.ToLongTimeString());
