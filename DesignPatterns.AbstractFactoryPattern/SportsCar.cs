9124cf88ff8b6e1776cb660df1a771d64dc67c6a
BakhtiyarShamilzada <bakhtiyarkhsh@code.edu.az>
2023-08-15 01:40:04 +0400

    add abstract factory design pattern

---
+++ DesignPatterns.AbstractFactoryPattern/SportsCar.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.AbstractFactoryPattern
+{
+    public class SportsCar : ICar
+    {
+        public void GetDetails()
+        {
+            Console.WriteLine("Fetching SportsCar details");
+        }
+    }
+}
